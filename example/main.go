1aff170266545be9c719dc17c0e287622d65662f
Martin Åkesson <c0rner@users.noreply.github.com>
2015-01-17 18:56:12 +0100

    Added example usage of Session.User()

--- example/main.go
+++ example/main.go
@@ -11,8 +11,9 @@ import (
 func main() {
 	session := rego.NewSession("RegoBot/1.0")
 	page := session.Listing("r/worldnews/new")
-	page.SetLimit(10)
-	for i := 0; i < 2; i++ {
+	page.SetLimit(5)
+	for i := 1; i <= 2; i++ {
+		fmt.Printf("--- PAGE %.2d ---\n", i)
 		list, err := page.Previous()
 		if err != nil {
 			log.Printf("Error: %s", err)
@@ -22,5 +23,16 @@ func main() {
 			age := now.Sub(l.Created.Time())
 			fmt.Printf("Created %d minutes ago, Name: %s, Title: %s\n", int(age.Minutes()), l.Name, l.Title)
 		}
+	}
+
+	fmt.Printf("\n--- USER LISTING ---\n")
+	account, err := session.User("wil")
+	if err != nil {
+		log.Printf("Error: %s", err)
+	} else {
+		fmt.Printf("ID: %s\n", account.ID)
+		fmt.Printf("User: %s\n", account.Name)
+		fmt.Printf("Is a mod: %t\n", account.IsMod)
+		fmt.Printf("Comment karma: %d\n", account.CommentKarma)
 	}
 }
